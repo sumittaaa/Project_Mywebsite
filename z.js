c539859011f990acb48e92146013b46a608889a1
AaRrGguSs <sk.sumitta16@gmail.com>
2023-09-17 14:32:54 +0700

    first commit

---
+++ z.js
@@ -0,0 +1,13 @@
+let slideIndex = 1;
+
+
+
+function plusSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dot = document.getElementsByClassName("dot");
+
+  console.log(slides);
+  console.log(dot);
+}
+
+showSlides();
